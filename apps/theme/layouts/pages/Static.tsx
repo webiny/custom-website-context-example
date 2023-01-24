9b66e06b46ad57f33fc222ed680fb52c17e7eb8f
Pavel Denisjuk <pavel@webiny.com>
2023-01-24 11:42:26 +0100

    feat: add SecurityProvider to Static layout

--- apps/theme/layouts/pages/Static.tsx
+++ apps/theme/layouts/pages/Static.tsx
@@ -3,6 +3,7 @@ import { Header } from "./Static/Header";
 import { Footer } from "./Static/Footer";
 import styled from "@emotion/styled";
 import { Global, css } from "@emotion/core";
+import { SecurityProvider, useSecurity } from "@webiny/app-security";
 
 const globalStyles = css`
     html {
@@ -26,14 +27,48 @@ const Layout = styled.div`
     }
 `;
 
-const Static: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const PageContent: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const { identity, setIdentity } = useSecurity();
+
+    // Simulate user login
+    const login = () => {
+        setIdentity({
+            id: "123",
+            displayName: "Demo User",
+            type: "user",
+            logout() {
+                setIdentity(null);
+            }
+        });
+    };
+
     return (
         <Layout>
             <Global styles={globalStyles} />
             <Header />
-            <main>{children}</main>
+            <strong>User: {identity ? identity.displayName : "anonymous"}</strong>
+
+            {identity ? (
+                <button onClick={identity.logout}>Logout</button>
+            ) : (
+                <button onClick={login}>Login</button>
+            )}
+
+            {identity ? (
+                <main>{children}</main>
+            ) : (
+                <main>You&apos;re not authorized to view this content!</main>
+            )}
             <Footer />
         </Layout>
+    );
+};
+
+const Static: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    return (
+        <SecurityProvider>
+            <PageContent>{children}</PageContent>
+        </SecurityProvider>
     );
 };
 
